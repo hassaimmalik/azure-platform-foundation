a6df22ecddd44ceea3cfd1775e69fe101013cee2
Hassaim Malik <hmalik@typefi.com>
2026-02-10 10:14:20 +0000

    Added 50-awx

---
+++ infra/50-awx/variables.tf
@@ -0,0 +1,54 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "uksouth"
+}
+
+variable "env" {
+  type    = string
+  default = "sandbox"
+}
+
+variable "networking_rg_name" {
+  type    = string
+  default = "rg-platform-networking-sandbox"
+}
+
+variable "management_rg_name" {
+  type    = string
+  default = "rg-platform-management-sandbox"
+}
+
+variable "hub_vnet_name" {
+  type    = string
+  default = "vnet-hub-sandbox"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+# IMPORTANT: this path must exist on the GitHub runner.
+# We'll handle that by generating the key in the workflow and writing it to a file.
+variable "ssh_public_key_path" {
+  type    = string
+  default = "awx_id_rsa.pub"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B2s"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    env    = "sandbox"
+    system = "azure-platform-foundation"
+    owner  = "hassam"
+  }
+}
